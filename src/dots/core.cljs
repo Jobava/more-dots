b15053db0bc88ab38a157e764a0c1777f9ba331b
Jobava <jobaval10n@gmail.com>
2016-11-05 23:00:58 +0200

    second commit

--- src/dots/core.cljs
+++ src/dots/core.cljs
@@ -3,34 +3,59 @@
 
 (enable-console-print!)
 
-(println "This text is printed from src/dots/core.cljs. Go ahead and edit it and see reloading in action.")
+#_(println "This text is printed from src/dots/core.cljs. Go ahead and edit it and see reloading in action.")
 
-;; define your app data so that it doesn't get over-written on reload
-
-(defonce app-state (atom {:width 800
-                          :height 600
-                          :rows 100
-                          :cols 100
-                          :sep 20
-                          :radius 3
-}))
+(defonce app-state (atom {:width 640
+                          :height 480
+                          :rows 20
+                          :cols 20
+                          :sep 10
+                          :radius 3}))
 
 (defn hello-world []
   [:div {:name "wrapper"}
-  [:svg {:width (:width @app-state)
-         :height (:height @app-state)}
+   [:div {:id "width-height"}
+    [:label {:for "width-id"} "width"]
+    [:input {:id "width-id" :type "text" :value "640"}]
+    [:label {:for "height-id"} "height"]
+    [:input {:id "height-id" :type "text" :value "480"}]]
+   [:div {:id "rows-cols"}
+    [:label {:for "rows-id"} "rows"]
+    [:input {:id "rows-id" :type "text" :value "20"}]
+    [:label {:for "cols-id"} "cols"]
+    [:input {:id "cols-id" :type "text" :value "20"}]]
+   [:div {:id "sep-radius"}
+    [:label {:for "sep-id"} "sep"]
+    [:input {:id "sep-id" :type "text" :value "10"}]
+    [:label {:for "radius-id"} "radius"]
+    [:input {:id "radius-id" :type "text" :value "3"}]]
+   [:div {:id "button"}
+    [:input {:type "button" :value "Update"
+             :onClick (fn []
+                        #_(js/alert "Hello!")
+                        (let [newstate {:width (js/parseInt (js/getElementById "width-id"))
+                                        :height (js/parseInt (js/getElementById "height-id"))
+                                        :rows (js/parseInt (js/getElementById "rows-id"))
+                                        :cols (js/parseInt (js/getElementById "cols-id"))
+                                        :sep (js/parseInt (js/getElementById "sep-id"))
+                                        :radius (js/parseInt (js/getElementById "radius-id"))}]
+                          (!swap app-state assoc :width (:width newstate))
+                          (!swap app-state assoc :height (:height newstate))
+                          (!swap app-state assoc :rows (:rows newstate))
+                          (!swap app-state assoc :cols (:cols newstate))
+                          (!swap app-state assoc :sep (:sep newstate))
+                          (!swap app-state assoc :radius (:radius newstate))))}]]
+   [:svg {:width (:width @app-state)
+          :height (:height @app-state)}
     [:g
-      (for [x (range (:rows @app-state))
-            y (range (:cols @app-state))]
-           [:circle
-              {:cx (* (:sep @app-state) x)
-              :cy (* (:sep @app-state) y)
-              :r (:radius @app-state)
+     (for [x (range (:rows @app-state))
+           y (range (:cols @app-state))]
+       [:circle
+        {:cx (* (:sep @app-state) x)
+         :cy (* (:sep @app-state) y)
+         :r (:radius @app-state)
               ;;:stroke "black"
-              :stroke-width 10}]
-      )
-    ]
-  ]])
+         :stroke-width 10}])]]])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
